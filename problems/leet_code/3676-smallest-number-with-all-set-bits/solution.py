0c1507fa5c7d89f7078e584d8fc8eb263493295a
Srashti Dwivedi <dwivedisrashti6@gmail.com>
2025-10-29 17:12:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (87.29%)

---
+++ problems/leet_code/3676-smallest-number-with-all-set-bits/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def smallestNumber(self, n):
+        if n <= 1:
+            return 1
+        k = 1
+        while (1 << k) - 1 < n:
+            k += 1
+        return (1 << k) - 1
+
