08db38c8c688cb866872facf4bdca4eb17d56603
Srashti Dwivedi <dwivedisrashti6@gmail.com>
2025-11-05 08:29:02 +0000

    Sync LeetCode submission Runtime - 7 ms (57.06%), Memory - 13.7 MB (41.29%)

---
+++ problems/leet_code/0283-move-zeroes/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums):
+        pointer_for_zero = 0 
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[pointer_for_zero] = nums[pointer_for_zero], nums[i]
+                pointer_for_zero += 1 
