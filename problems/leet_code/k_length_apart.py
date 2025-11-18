e5541447509b73d08e452e8bccc3e20f58422047
Srashti Dwivedi <dwivedisrashti6@gmail.com>
2025-11-18 17:39:00 +0000

    Solved one bit character problem

---
+++ problems/leet_code/k_length_apart.py
@@ -0,0 +1,15 @@
+def kLengthApart(nums, k):
+    distance = 0
+    found = False
+    
+    for num in nums:
+        if num == 1:
+            
+            if distance <= k and found:
+                return False 
+            
+            found = True
+            distance = 0
+        
+        distance += 1
+    return True
