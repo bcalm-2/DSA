7f3fa95659b3b73abdfe5ec480d5b49d489c048a
Srashti Dwivedi <dwivedisrashti6@gmail.com>
2025-11-23 11:07:21 +0000

    Solved String related problems

---
+++ problems/leet_code/maximum_occurence_character.py
@@ -0,0 +1,10 @@
+def getMaxOccurringChar(s):
+    ordered_letters = list(set(s))
+    ordered_letters.sort()
+    maxx = 0
+    res = ''
+    for i in ordered_letters:
+        if s.count(i) > maxx:
+            maxx = s.count(i)
+            res =  i
+    return res
---
+++ problems/leet_code/reverse_words.py
@@ -0,0 +1,6 @@
+def reverseWords(s):
+    k=s.split()
+    k=k[::-1]
+    k=" ".join(k)
+    return k
+        
