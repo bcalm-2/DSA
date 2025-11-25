af78eabbe34f7f02b98874fbbaa1915ffbec3464
Srashti Dwivedi <dwivedisrashti6@gmail.com>
2025-11-25 16:51:36 +0000

    Sync LeetCode submission Runtime - 7 ms (84.63%), Memory - 14 MB (37.82%)

---
+++ problems/leet_code/1064-smallest-integer-divisible-by-k/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def smallestRepunitDivByK(self, k):
+        if k % 2 == 0 or k % 5 == 0: return -1
+        r = 0
+        for i in range(1, k+1):
+            r = (r * 10 + 1) % k
+            if r == 0: return i
+        return -1
