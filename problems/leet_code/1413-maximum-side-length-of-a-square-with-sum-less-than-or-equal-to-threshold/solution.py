30c5ca986846b7f068bbf0d4aa5c876368a4ad05
Srashti Dwivedi <dwivedisrashti6@gmail.com>
2026-01-19 14:27:44 +0000

    Sync LeetCode submission Runtime - 287 ms (81.82%), Memory - 17 MB (72.73%)

---
+++ problems/leet_code/1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/solution.py
@@ -0,0 +1,38 @@
+#Mp
+class Solution(object):
+    def maxSideLength(self, mat, threshold):
+        m = len(mat)
+        n = len(mat[0])
+        prefix = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                prefix[i][j] = (
+                    mat[i - 1][j - 1]
+                    + prefix[i - 1][j]
+                    + prefix[i][j - 1]
+                    - prefix[i - 1][j - 1]
+                )
+        def check(k):
+            for i in range(k, m + 1):
+                for j in range(k, n + 1):
+                    square_sum = (
+                        prefix[i][j]
+                        - prefix[i - k][j]
+                        - prefix[i][j - k]
+                        + prefix[i - k][j - k]
+                    )
+                    if square_sum <= threshold:
+                        return True
+            return False
+        left, right = 1, min(m, n)
+        ans = 0
+        while left <= right:
+            mid = (left + right) // 2
+            if check(mid):
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return ans
